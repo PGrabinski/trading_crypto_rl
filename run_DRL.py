caa689493a19d919e6fa0e374ca278c80126a70b
Paweł Grabiński <266719@uwr.edu.pl>
2022-09-01 15:40:02 +0200

    Changed timeseries.py in pyfolio.

--- run_DRL.py
+++ run_DRL.py
@@ -25,7 +25,6 @@ validation_window = 30
 
 def run_model() -> None:
     """Train the model."""
-    print('start run')
     # read and preprocess data
     preprocessed_path = "done_data.csv"
     if os.path.exists(preprocessed_path):
@@ -35,14 +34,14 @@ def run_model() -> None:
         data = add_turbulence(data)
         data.to_csv(preprocessed_path)
 
-    print(data.head())
-    print(data.size)
+    # print(data.head())
+    # print(data.size)
 
     # 2015/10/01 is the date that validation starts
     # 2016/01/01 is the date that real trading starts
     # unique_trade_date needs to start from 2015/10/01 for validation purpose
     unique_trade_date = data[(data.datadate > BEGINING_TEST_DATE)&(data.datadate <= END_TEST_DATE)].datadate.unique()
-    print(unique_trade_date)
+    # print(unique_trade_date)
 
     # rebalance_window is the number of months to retrain the model
     # validation_window is the number of months to validation the model and select for trading
